e998f106952c4ea623b3b30bfd618f9ce47dd91e
emousse <etienne.mousse@gmail.com>
2021-04-18 10:43:33 +0200

    fixs'

--- notify.js
+++ notify.js
@@ -1,16 +1,22 @@
-const libnotify = require('node-notifier')
+//const libnotify = require('node-notifier')
+const PushBullet = require('pushbullet');
+const pusher = new PushBullet(`${process.env.PUSHBULLET_API}`);
 
 function notify(diffPixels) {
   if (diffPixels > 0) {
-    libnotify.notify({
-      title: 'Site Checker',
-      message: `${process.env.SITEURL} has changed! GAAAAAAAAH!`,
-      open: `${process.env.SITEURL}`,
-      sound: true
-    })
+    /*     libnotify.notify({
+          title: 'Site Checker',
+          message: `${process.env.SITEURL} has changed! GAAAAAAAAH!`,
+          open: `${process.env.SITEURL}`,
+          sound: true
+        }) */
+    pusher.link(`${process.env.PUSHBULLET_EMAIL}`, 'Site Checker', `${process.env.SITEURL}`, `${process.env.SITEURL} has changed! GAAAAAAAAH!`, function (error, response) { console.log(response); });
+
   }
   else {
     console.log('nothing change !');
+    
+
   }
 }
 
